2dcb40f8c78afb895a8c71cd35e1f1068621954b
mamrmohamedabdelaziz@gmail.com <mamrmohamedabdelaziz@gmail.com>
2025-10-03 16:02:04 +0300

    Ceaser cypher

---
+++ CeaserKey.py
@@ -0,0 +1,10 @@
+def decryptCeaser(text, shift): 
+    Charachters = list(text.upper())
+    decrypted = ""
+    for char in Charachters:
+        current = ord(char)
+        updated = (ord(char) - 65 - shift) %26 +65
+        letter = chr(updated)
+        decrypted += letter
+    return decrypted
+
